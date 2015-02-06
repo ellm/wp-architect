a0a911394b5e5fff21240044fd34f58d6494624c
Matthew Ell <ell.matthew@gmail.com>
2015-02-06 17:15:52 -0500

    Switch to LibSass  - Using Breakpoint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,103 +1,23 @@
 'use strict';
 
 module.exports = function(grunt) {
-    // Load all tasks
-    require('load-grunt-tasks')(grunt);
     // Show elapsed time
-    require('time-grunt')(grunt);
+    // require('time-grunt')(grunt);
 
     grunt.initConfig({
+    	pkg: grunt.file.readJSON('package.json'),
 
-        //autoprefixer
-        autoprefixer: {
-          options: {
-            browsers: ['Android >= 2.1', 'Chrome >= 21', 'Explorer >= 8', 'Firefox >= 17', 'Opera >= 12.1', 'Safari >= 6.0']
-          },
-          dist: {
-            options: {
-              map: {
-                prev: 'assets/css/'
-              }
-            },
-            src: 'assets/css/global.css'
-          }
-        },
-
-        svg2png: {
-            all: {
-                // specify files in array format with multiple src-dest mapping
-                files: [
-                    // rasterize all SVG files in "img" and its subdirectories to "img/png"
-                    // { src: ['assets/img/**/*.svg'], dest: 'img/png/' },
-                    // rasterize SVG file to same directory
-                    { src: ['assets/img/*.svg'] }
-                ]
-            }
-        },
-
+        //
         sass: {
+	        options: {
+	        	require: 'susy',
+	            sourceMap: true,
+				outputStyle: 'compressed'
+	        },
             dev: {
-                options: {
-                    style: 'expanded',
-                    require: 'susy'
-                },
                 files: {
                     'assets/css/global.css': 'assets/scss/global.scss'
                 }
-            }
-        },
-
-        // Minify CSS
-        cssmin: {
-            options: {
-                banner: '/*! Processed <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> */\n'
-            },
-            combine: {
-                files: {
-                  'assets/css/global.min.css': ['bower_components/normalize-css/normalize.css','assets/css/global.css'],
-                },
-            },
-        },
-
-        // Uglify/Minification for JS files
-        uglify: {
-            options: {
-                banner: '/*! Processed <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> */\n'
-            },
-            build: {
-                src: ['assets/js/common.js'],
-                dest: 'assets/js/common.min.js'
-            }
-        },
-
-        // Image Minification
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'assets/img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'assets/img/'
-                }]
-            }
-        },
-
-        // Modernizr Grunt - custom modernizr build.
-        modernizr: {
-            build: {
-                devFile: 'bower_components/modernizr/modernizr.js',
-                outputFile: 'assets/js/modernizr.min.js',
-                files : {
-                    'src' : [
-                        ['assets/scss/*.scss'],
-                        ['assets/scss/**/*.scss'],
-                        ['assets/js/common.js'],
-                        ['assets/js/src/*.js'],
-                        ['assets/js/src/**/*.js']
-                    ]
-                },
-                uglify : true,
-                parseFiles : true,
             }
         },
 
@@ -105,7 +25,8 @@ module.exports = function(grunt) {
         watch: {
 
             options: {
-                livereload: true
+                livereload: true,
+                nospawn: true
             },
 
             html: {
@@ -124,22 +45,13 @@ module.exports = function(grunt) {
         }
     });
 
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks("grunt-modernizr");
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-newer');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-svg2png');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Register Tasks
     grunt.registerTask('default', [
-        'sass',
-        'uglify',
-        'modernizr',
-        'newer:cssmin'
+        'sass'
     ]);
 
     grunt.registerTask('dev', [
